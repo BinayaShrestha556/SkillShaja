c58df97e2659863edb6724e70bea4a3b230b2d79
BinayaShrestha556 <binayastha37b@gmail.com>
2025-07-04 08:41:28 +0545

    navbar search bar changes

--- components/navbar/main.tsx
+++ components/navbar/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="w-full h-16 px-10 flex items-center justify-between z-50  absolute top-0 left-0 right-0">
-      <div className="flex items-center gap-5">
-        <h1 className="text-3xl font-bold text-nowrap"> Logo here </h1>
+      <div className="flex items-center gap-5 relative">
+        <h1 className="text-3xl font-bold text-nowrap text-primary">
+          {" "}
+          Logo here{" "}
+        </h1>
         <ul className="flex  gap-5">
           <li className="hover:text-primary hover:underline cursor-pointer">
             about
@@ -17,8 +21,12 @@ const Navbar = () => {
         </ul>
         <Input
           placeholder="Search anything"
-          className="rounded-full w-80 backdrop-blur-xl bg-white border"
+          className="rounded-full p-5 w-80 backdrop-blur-xl bg-white border"
         />
+        <FaSearch
+          size={10}
+          className="w-8 h-8  p-1.5 cursor-pointer absolute right-[5px] rounded-full bg-primary text-white"
+        />{" "}
       </div>
       <div className="flex items-center gap-3 ">
         <a href="/signin">
